3d9b09d55020948ef9d6f3c0c724cec567304438
denbell <denbell@ukr.net>
2020-09-26 12:53:30 +0300

    Refactor Service1

--- Task_Queue/Service1.cs
+++ Task_Queue/Service1.cs
@@ -26,87 +26,48 @@ namespace Task_Queue
 		{
 			InitializeComponent();
 
-			this.logger = new Logger();
-			this.context = new TaskDbContext();
+			logger = new Logger();
+			context = new TaskDbContext();
 		}
 
 		protected override void OnStart(string[] args)
 		{
-			this.logger.Log("Service started");
-			this.claimTimer = new Timer(5000);
-			this.claimTimer.Elapsed += (s, e) =>
+			logger.Log("Service started");
+			claimTimer = new Timer(5000);
+			claimTimer.Elapsed += (s, e) =>
 			{
-				this.logger.Log("claimTimer elapsed event");
-				this.CheckClaims();
+				logger.Log("claimTimer elapsed event");
+				CheckClaims();
 			};
-			this.claimTimer.Start();
+			claimTimer.Start();
 		}
 
 		protected override void OnStop()
 		{
-			this.claimTimer.Stop();
-			this.logger.Log("Service stopped");
+			claimTimer.Stop();
+			logger.Log("Service stopped");
 		}
 
-
-		private void StartBackgroundWork(object obj)
+		private void OnProgressChanged(WorkerWrapper worker, ProgressChangedEventArgs e)
 		{
-			BackgroundWorker worker = new BackgroundWorker();
-			worker.WorkerReportsProgress = true;
-			worker.WorkerSupportsCancellation = true;
-			worker.DoWork += DoWork;
-			worker.ProgressChanged += OnProgressChanged;
-			worker.RunWorkerCompleted += OnRunWorkerCompleted;
-			worker.RunWorkerAsync(obj);
+			logger.Log($"{worker.Task.Name} {worker.Task.Progress}");
 		}
 
-		private void DoWork(object sender, DoWorkEventArgs e)
-		{
-			this.logger.Log($"Work {this.counter} started");
-
-			var worker = sender as BackgroundWorker;
-			var task = e.Argument as CustomTask;
-			task.Status = CustomTaskStatus.InProgress;
-
-			for (int i = 1; i <= 10; i++)
-			{
-				if (worker.CancellationPending == true)
-				{
-					e.Cancel = true;
-					break;
-				}
-				else
-				{
-					// Perform a time consuming operation and report progress.
-					System.Threading.Thread.Sleep(500);
-					task.Progress = i * 10;
-					worker.ReportProgress(task.Progress, task);
-				}
-			}
-
-			e.Result = task;
-		}
-
-		private void OnProgressChanged(object sender, ProgressChangedEventArgs e)
-		{
-			this.logger.Log($"{(e.UserState as CustomTask).Name} {e.ProgressPercentage}");
-		}
-
-		private void OnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		private void OnRunWorkerCompleted(WorkerWrapper worker, RunWorkerCompletedEventArgs e)
 		{
 			if (e.Cancelled == true)
 			{
-				this.logger.Log("Canceled!");
+				logger.Log("Canceled!");
 			}
 			else if (e.Error != null)
 			{
-				this.logger.Log("Error: " + e.Error.Message);
-				this.logger.Log("Error: " + e.Error.StackTrace);
+				logger.Log("Error: " + e.Error.Message);
+				logger.Log("Error: " + e.Error.StackTrace);
 			}
 			else
 			{
-				this.logger.Log("Done!");
-				this.logger.Log(e.Result.GetHashCode().ToString());
+				logger.Log("Done!");
+				logger.Log(e.Result.GetHashCode().ToString());
 			}
 
 			var completedTask = e.Result as CustomTask;
@@ -119,23 +80,22 @@ namespace Task_Queue
 
 		private void CheckClaims()
 		{
-			// look in db
-			
-			// add new task to Tasks
-			// this.CheckTasks()
+			var earliestDate = context.TaskClaims.Min(
+				claim => claim.CreatedAt
+			);
 
-			var earliestDate = this.context.TaskClaims
-				.Min(claim => claim.CreatedAt);
-			var earliestClaim = this.context.TaskClaims
-				.FirstOrDefault(claim => claim.CreatedAt == earliestDate);
+			var earliestClaim = context.TaskClaims.FirstOrDefault(
+				claim => claim.CreatedAt == earliestDate
+			);
 
-			this.context.TaskClaims = this.context.TaskClaims
-				.Where(claim => claim.Id != earliestClaim.Id)
-				.ToList();
-			// TODO: check claim null
+			if (earliestClaim == null)
+			{
+				logger.Log($"No claims in database.");
+				return;
+			}
 
-			this.logger.Log($"Got earliest Claim {earliestClaim.Claim}");
-
+			logger.Log($"Got earliest Claim {earliestClaim.Claim}");
+			context.TaskClaims.Remove(earliestClaim);
 			// TODO: check claim name syntax
 
 			var newTask = new CustomTask
@@ -143,53 +103,60 @@ namespace Task_Queue
 				Id = Guid.NewGuid(),
 				Name = earliestClaim.Claim,
 				Priority = Convert.ToInt32(earliestClaim.Claim.Replace("Task_", "")),
-				Status = Data.Models.Enums.CustomTaskStatus.Queued
+				Status = CustomTaskStatus.Queued
 			};
 
-			this.context.CustomTasks = this.context.CustomTasks.Append(newTask);
+			context.CustomTasks.Add(newTask);
 
-			this.logger.Log("Before CheckTasks");
-			this.CheckTasks();
+			logger.Log("Before CheckTasks");
+			CheckTasks();
 		}
 
 		private void CheckTasks()
 		{
-			this.logger.Log("In CheckTasks");
+			logger.Log("In CheckTasks");
 			// look in db
 			// check tasks that are in progress
 			// if slots are available, get highest priority task
 			// start working on that task
 			//
-			var inProgressTaskCount = this.context.CustomTasks
-				.Count(task => task.Status == CustomTaskStatus.InProgress);
+			var inProgressTaskCount = context.CustomTasks.Count(
+				task => task.Status == CustomTaskStatus.InProgress
+			);
 
-			if(inProgressTaskCount > 0)
+			if (inProgressTaskCount > 0)
 			{
-				this.logger.Log("No available slots for new task to work.");
+				logger.Log("No available slots for new task to work.");
 				return;
 			}
 
-			var highestPriorityTask = this.GetHighestPriorityTask();
+			var highestPriorityTask = GetHighestPriorityTask();
 
 			if(highestPriorityTask == null)
 			{
-				this.logger.Log("highestPriorityTask is null.");
+				logger.Log("highestPriorityTask is null.");
 				return;
 			}
 
-			this.logger.Log($"Got {highestPriorityTask.Name} to work.");
-			this.StartBackgroundWork(highestPriorityTask);
+			logger.Log($"Got {highestPriorityTask.Name} to work.");
+			WorkerWrapper worker = new WorkerWrapper(highestPriorityTask);
+			worker.ProgressChanged += OnProgressChanged;
+			worker.WorkCompleted += OnRunWorkerCompleted;
+			worker.StartWork();
+			logger.Log($"Task {worker.Task} started");
 		}
 
 		private CustomTask GetHighestPriorityTask()
 		{
-			var queuedTasks = this.context.CustomTasks
-				.Where(task => task.Status == CustomTaskStatus.Queued);
+			var queuedTasks = this.context.CustomTasks.Where(
+				task => task.Status == CustomTaskStatus.Queued
+			);
 
 			var highestPriority = queuedTasks.Min(task => task.Priority);
 
-			var highestPriorityTask = queuedTasks
-				.FirstOrDefault(task => task.Priority == highestPriority);
+			var highestPriorityTask = queuedTasks.FirstOrDefault(
+				task => task.Priority == highestPriority
+			);
 
 			return highestPriorityTask;
 		}
